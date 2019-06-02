267c6e0c5e0ca0bc6e1ecf1dd6f75eee51b68fa8
Bo Zhang <bo@gradle.com>
2019-06-02 11:37:10 +0800

    Update problems

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Java的数据类型练习：数据类型之间的转换
 
-请完成[`Cast`这个类](https://github.com/hcsp/data-type-casts/blob/master/src/main/java/com/github/datatype/Cast.java)中的所有转换方法。
+请完成[`Cast`这个类](https://github.com/hcsp/data-type-casts/blob/master/src/main/java/com/github/hcsp/datatype/Cast.java)中的所有转换方法。
 
 在提交Pull Request之前，你应当在本地确保所有代码已经编译通过，并且通过了测试(`mvn test`)
 
 -----
 注意！我们只允许你修改以下文件，对其他文件的修改会被拒绝：
-- [src/main/java/com/github/datatype/Cast.java](https://github.com/hcsp/data-type-casts/blob/master/src/main/java/com/github/datatype/Cast.java)
+- [src/main/java/com/github/hcsp/datatype/Cast.java](https://github.com/hcsp/data-type-casts/blob/master/src/main/java/com/github/hcsp/datatype/Cast.java)
 -----
 
 
--- src/test/java/com/github/hcsp/datatype/CastTest.java
+++ src/test/java/com/github/hcsp/datatype/CastTest.java
@@ -1,26 +1,24 @@
-
 package com.github.hcsp.datatype;
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
 
 class CastTest {
-@Test
-public void castTest() {
-    assertEquals(1, Cast.int2byte(1));
-    assertEquals(1, Cast.int2short(1));
-    assertEquals(1, Cast.int2char(1));
-    assertEquals("123", Cast.int2String(123));
-    assertEquals("-123", Cast.int2String(-123));
-    assertEquals(123, Cast.int2String("123"));
-    assertEquals(-123, Cast.int2String("-123"));
-    assertEquals(1, Cast.int2String("0x1"));
-    assertEquals(1, Cast.int2String("0b1"));
-    assertEquals(1, Cast.int2String("0e1"));
-    assertTrue(Math.abs(0.5 - Cast.string2double("0.5")) < 1e-5)
-    assertTrue(Math.abs(0.123 - Cast.string2double("0.123")) < 1e-5)
-    assertTrue(Math.abs(1e-5 - Cast.string2double("1e-5")) < 1e-5)
-}
-
+    @Test
+    public void castTest() {
+        assertEquals(1, Cast.int2byte(1));
+        assertEquals(1, Cast.int2short(1));
+        assertEquals(1, Cast.int2char(1));
+        assertEquals("123", Cast.int2String(123));
+        assertEquals("-123", Cast.int2String(-123));
+        assertEquals(123, Cast.string2int("123"));
+        assertEquals(-123, Cast.string2int("-123"));
+        assertEquals(123, Cast.Long2int(123L));
+        assertEquals(123L, Cast.double2Long(123d));
+        assertTrue(Math.abs(0.5 - Cast.string2double("0.5")) < 1e-5);
+        assertTrue(Math.abs(0.123 - Cast.string2double("0.123")) < 1e-5);
+        assertTrue(Math.abs(1e-5 - Cast.string2double("1e-5")) < 1e-5);
+    }
 }
