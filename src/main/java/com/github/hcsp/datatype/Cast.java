b2aa2d328b96aab2de39fbea619a4cb717961260
frank-0908 <2059326740@qq.com>
2019-07-13 21:47:18 +0800

    数据类型之间的转换

--- src/main/java/com/github/hcsp/datatype/Cast.java
+++ src/main/java/com/github/hcsp/datatype/Cast.java
@@ -4,69 +4,56 @@ public class Cast {
     // Cast an int to byte
     // 将int类型转换成byte
     public static byte int2byte(int i) {
-        int a=10;
-        byte i=(byte) a;
-        return i;
+        return (byte) i;
     }
 
     // Cast an int to short
     // 将int类型转换成short
     public static short int2short(int i) {
-        int a=10;
-        short i=(short)a;
-        return i;
+        return (short)i;
     }
 
     // Cast an int to char
     // 将int类型转换成char
     public static char int2char(int i) {
-        char a=10;
-        char i=(char) a;
-        return i;
+        return (char) i;
     }
 
     // Cast an int to String, e.g. 123 -> "123"
     // 将一个整数转换为字符串，例如，将123转换成字符串"123"
     public static String int2String(int i) {
-        int i=123;
-        String a="123";
-        String i=(String) a;
-        return i;
+        i=123;
+        String a=String.valueOf(i);
+        return  a;
     }
 
     // Cast an String to int, e.g. "123" -> 123
     // 将一个字符串转换成整数，例如，将字符串"123"转换成整数123
     public static int string2int(String s) {
         s="123";
-        int i=123;
-        int s=(int) i;
-        return s;
+        int a=string2int(s);
+        return a;
     }
 
     // Cast an String to double, e.g. "1.23" -> 1.23
     // 将一个字符串转换成double类型，例如，将字符串"1.23"转换成1.23
     public static double string2double(String s) {
         s="1.23";
-        double a=1.23;
-        double s=(double) a;
-        return s;
+        double a=string2double(s);
+        return a;
     }
 
     // Cast an Long to int
     // 将Long类型转换成int
     public static int Long2int(Long l) {
-        l=234l;
-        int i=21;
-        int l=(int) i;
-        return l;
+        int a=Long2int(l);
+        return a;
     }
 
     // Cast an Double to long
     // 将Double类型转换成long
     public static long double2Long(Double d) {
-        d=2233.1;
-        long a=32l;
-        long d=(long) a;
-        return d;
+        long a=double2Long(d);
+        return a;
     }
 }
