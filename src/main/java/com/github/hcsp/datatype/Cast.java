aed67602a1abbec9fe1d8a1f3fa360f6f3df13a8
frank-0908 <2059326740@qq.com>
2019-07-14 19:48:18 +0800

    数据类型之间的转换

--- src/main/java/com/github/hcsp/datatype/Cast.java
+++ src/main/java/com/github/hcsp/datatype/Cast.java
@@ -36,7 +36,7 @@ public class Cast {
     // Cast an String to double, e.g. "1.23" -> 1.23
     // 将一个字符串转换成double类型，例如，将字符串"1.23"转换成1.23
     public static double string2double(String s) {
-        double a=s.hashCode();
+        double a=Double.valueOf(s);
         return a;
     }
 
