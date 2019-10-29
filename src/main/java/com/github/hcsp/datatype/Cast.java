ef20744340a76e7d3497540657213480897b9643
Ditto0 <46198155+Ditto0@users.noreply.github.com>
2019-10-30 00:19:48 +0800

    2

--- src/main/java/com/github/hcsp/datatype/Cast.java
+++ src/main/java/com/github/hcsp/datatype/Cast.java
@@ -27,19 +27,19 @@ public class Cast {
     // 将一个整数转换为字符串，例如，将123转换成字符串"123"
     public static String int2String(int i) {
 
-        return String.valueOf(i);
+        return Integer.toString(i);
     }
 
     // Cast an String to int, e.g. "123" -> 123
     // 将一个字符串转换成整数，例如，将字符串"123"转换成整数123
     public static int string2int(String s) {
-        return Integer.parseInt(s);
+            return Integer.valueOf(s);
     }
 
     // Cast an String to double, e.g. "1.23" -> 1.23
     // 将一个字符串转换成double类型，例如，将字符串"1.23"转换成1.23
     public static double string2double(String s) {
-        return Double.parseDouble(s);
+        return Double.valueOf(s);
     }
 
     // Cast an Long to int
