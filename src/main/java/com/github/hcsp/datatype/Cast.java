9ec22d375f494d043b35c2d7798acf8aac52cae2
DEMAO <987854018@qq.com>
2021-09-24 11:15:52 +0800

    数据类型之间的转换2

--- src/main/java/com/github/hcsp/datatype/Cast.java
+++ src/main/java/com/github/hcsp/datatype/Cast.java
@@ -40,14 +40,14 @@ public class Cast {
     // Cast an Long to int
     // 将Long类型转换成int
     public static int Long2int(Long l) {
-        long bb = l;
-        return (int) bb;
+        long z = l;
+        return (int) z;
     }
 
     // Cast an Double to long
     // 将Double类型转换成long
     public static long double2Long(Double d) {
-        double aa = d;
-        return (long) aa;
+        double m = d;
+        return (long) m;
     }
 }
