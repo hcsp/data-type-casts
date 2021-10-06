bd096e7619cdea4bc1003d606ea75c5f95a6e644
thomasz <2596889532@qq.com>
2021-10-06 16:00:30 +0800

    完成数据类型间的转换方法

--- src/main/java/com/github/hcsp/datatype/Cast.java
+++ src/main/java/com/github/hcsp/datatype/Cast.java
@@ -4,20 +4,19 @@ public class Cast {
     // Cast an int to byte
     // 将int类型转换成byte
     public static byte int2byte(int i) {
-
-        return (byte)i;
+        return (byte) i;
     }
 
     // Cast an int to short
     // 将int类型转换成short
     public static short int2short(int i) {
-        return (short)i;
+        return (short) i;
     }
 
     // Cast an int to char
     // 将int类型转换成char
     public static char int2char(int i) {
-        return (char)i;
+        return (char) i;
     }
 
     // Cast an int to String, e.g. 123 -> "123"
@@ -41,12 +40,12 @@ public class Cast {
     // Cast an Long to int
     // 将Long类型转换成int
     public static int Long2int(Long l) {
-        return l.intValue();
+        return Integer.parseInt(String.valueOf(l));
     }
 
     // Cast an Double to long
     // 将Double类型转换成long
     public static long double2Long(Double d) {
-        return Long.parseLong(String.valueOf(d));
+        return new Double(d).longValue();
     }
 }
