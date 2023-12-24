d5ea5b7658198d93c17bcd41b9a81dec24f5d4c2
buuoltwo <bullnick20@gmail.com>
2023-12-24 21:18:38 +0800

    Revert "feat2"
    This reverts commit 0288ecc63f83bd44a5d87e19db5b3c82c7e85f81.

--- src/test/java/com/github/hcsp/datatype/CastTest.java
+++ src/test/java/com/github/hcsp/datatype/CastTest.java
@@ -13,7 +13,7 @@ public class CastTest {
         Assertions.assertEquals("-123", Cast.int2String(-123));
         Assertions.assertEquals(123, Cast.string2int("123"));
         Assertions.assertEquals(-123, Cast.string2int("-123"));
-        Assertions.assertEquals(123, Cast.long2int(123L));
+        Assertions.assertEquals(123, Cast.Long2int(123L));
         Assertions.assertEquals(123L, Cast.double2Long(123d));
         Assertions.assertTrue(Math.abs(0.5 - Cast.string2double("0.5")) < 1e-5);
         Assertions.assertTrue(Math.abs(0.123 - Cast.string2double("0.123")) < 1e-5);
